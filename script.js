f03c7c9bf7d807b7600b0c75257cec8c0c17def0
Pedro Bianchi <pedro.bianchi0208@gmail.com>
2026-02-19 16:26:30 -0300

    Add loading antes de abrir a pagina

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-const downloadBtn = document.getElementById('downloadBtn');
-
-downloadBtn.addEventListener('click', () => {
-    console.log("Jack-in sequence initiated!");
+window.addEventListener("load", function() {
+    const loader = document.getElementById("loading-screen");
+    
+    if (loader) {
+        setTimeout(() => {
+            loader.classList.add("loader-hidden");
+        }, 1000); // 1 segundo para mostrar a animação
+    }
 });
 
-// Efeito Parallax no Título (Desktop)
+// Força o fechamento após 5 segundos caso o evento 'load' falhe
+setTimeout(() => {
+    const loader = document.getElementById("loading-screen");
+    if (loader && !loader.classList.contains("loader-hidden")) {
+        loader.classList.add("loader-hidden");
+    }
+}, 5000);
+
+// Efeito Parallax no Título
 if (!('ontouchstart' in window)) {
     document.addEventListener('mousemove', (e) => {
         const title = document.querySelector('h1');
         if(title) {
-            const x = (window.innerWidth / 2 - e.pageX) / 65;
-            const y = (window.innerHeight / 2 - e.pageY) / 65;
+            const x = (window.innerWidth / 2 - e.pageX) / 70;
+            const y = (window.innerHeight / 2 - e.pageY) / 70;
             title.style.transform = `translate(${x}px, ${y}px)`;
         }
     });
